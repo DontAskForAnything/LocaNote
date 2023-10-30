21b4aba01ac849c921e031063305bae9830bb267
Kasowy_Code <maciej.smirski@hotmail.com>
2023-10-30 22:04:47 +0100

    Needs review flashcards get pushed back into the queue

--- src/screens/main/welcome.tsx
+++ src/screens/main/welcome.tsx
@@ -19,7 +19,20 @@ import { Flashcard, SubjectItem } from "../../utils/types";
 import LogoTop from "../../components/logoTop";
 
 type SubjectObject = SubjectItem[] | [];
-const flashcards: Array<Flashcard> = [{question: "What is?", answer: "It is indeed"}, {question: "What was?", answer: "Yes, It was"}];
+const flashcards: Array<Flashcard> = [
+  {
+    question:
+      "What are the two major groups of modern turtles and how do they differ in head retraction?",
+    answer:
+      "Modern turtles are divided into two major groups, the Pleurodira (side necked turtles) and Cryptodira (hidden necked turtles). They differ in the way the head retracts.",
+  },
+  { question: "What was?", answer: "Yes, It was" },
+  {
+    question: "This is a testing question made to test the testing system",
+    answer:
+      "This is the answer to the testing question that was made to test the testing system",
+  },
+];
 export const MainScreen = (params: RootStackScreenProps<"MainScreen">) => {
   const { user, isLoaded } = useUser();
   const [subjects, setSubjects] = useState<SubjectObject>([]);
